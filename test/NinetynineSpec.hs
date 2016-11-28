8ebdeea5e76f5bbd80e2505d894ec17279a70c4a
Jongsoo Lee <leejongsoo@gmail.com>
2016-11-28 23:28:04 +0900

    update test feature
    * add some hspec tutorial code
    * add test launcher

--- test/NinetynineSpec.hs
+++ test/NinetynineSpec.hs
@@ -1,12 +1,38 @@
-module NinetynineSpec (spec) where
+ {-# OPTIONS_GHC -Wno-type-defaults #-}
+
+module NinetynineSpec (test, spec) where
 
 import Test.Hspec
 import Test.QuickCheck
 
+import Control.Exception
+
 import Ninetynine
 
+-- Hspec: http://hspec.github.io/index.html
+-- QuickCheck: http://www.cse.chalmers.se/~rjmh/QuickCheck/manual.html
+
+test :: IO ()
+test = hspec spec
+
 spec :: Spec
-spec = do
+spec = parallel $ do
+  describe "hspec tutorial" $ do
+    it "can parse integers" $ do
+      read "10" `shouldBe` (10 :: Int)
+    it "pending test" $ do
+      pending
+      pendingWith "need to fix with message"
+    it "expecting equality" $ do
+      (+1) 0 `shouldBe` 1
+    it "require that a predicate holds" $ do
+      1 `shouldSatisfy` (== 1)
+    it "expecting exceptions" $ do
+      -- anyException, anyErrorCall, anyIOException, anyArithException
+      evaluate (head []) `shouldThrow` anyException
+      evaluate (1 `div` 0) `shouldThrow` anyArithException
+
+
   describe "myLast" $ do
     it "find the last element of a list" $ do
       myLast [1,2,3,4] `shouldBe` 4
@@ -26,6 +52,7 @@ spec = do
   describe "flatten" $ do
     it "Flatten a nested list structure" $ do
       flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]]) `shouldBe` [1,2,3,4,5]
---      flatten (List []) `shouldBe` []
---      flatten (Elem 5) `shouldBe` [5]
+      flatten (Elem 5) `shouldBe` [5]
+    it "Flatten a empty list structure" $ do
+      (flatten (List []) :: [Integer]) `shouldBe` ([] :: [Integer])
 
